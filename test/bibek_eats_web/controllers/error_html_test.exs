4164ede0b0d507d6c423e6f77fa5fc75fae6e131
Daniel Rogowski <drogovski@proton.me>
2024-06-21 16:07:06 +0200

    delete test

--- test/bibek_eats_web/controllers/error_html_test.exs
+++ test/bibek_eats_web/controllers/error_html_test.exs
@@ -7,8 +7,4 @@ defmodule BibekEatsWeb.ErrorHTMLTest do
   test "renders 404.html" do
     assert render_to_string(BibekEatsWeb.ErrorHTML, "404", "html", []) == "Not Found"
   end
-
-  test "renders 500.html" do
-    assert render_to_string(BibekEatsWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
-  end
 end
